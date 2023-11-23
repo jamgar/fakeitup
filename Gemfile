b1e402a7d03d3041e586b7ac2ba76a77072b3969
github username <github email address>
2023-11-22 18:28:49 -0600

    add authentication-zero gem and letter_opener

--- Gemfile
+++ Gemfile
@@ -57,7 +57,7 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
-
+  gem "letter_opener", "~> 1.8"
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
@@ -71,4 +71,9 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+
+gem "authentication-zero", "~> 3.0"
+# Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
+gem "bcrypt", "~> 3.1.7"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
+  get  "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  get  "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  resources :sessions, only: [:index, :show, :destroy]
+  resource  :password, only: [:edit, :update]
+  namespace :identity do
+    resource :email,              only: [:edit, :update]
+    resource :email_verification, only: [:show, :create]
+    resource :password_reset,     only: [:new, :edit, :create, :update]
+  end
+  root "home#index"
+  # root "static_pages#home"
 
   resources :fake_sets
   resources :fake_set_types, only: [], param: :index do
